ce613c6fff7ae931b7a27779689f3b9c5b4a59f9
AngeOrtiz <angebtes@gmail.com>
2025-09-04 11:20:03 -0600

    Arreglar main

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      transform: true,                        //  convierte "false"/"0" en false
-      transformOptions: { enableImplicitConversion: true },
-    }),
-  );
-
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
