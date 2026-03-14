25c06c6b35195c1c6b9b3183c06f197311f8acac
AngeOrtiz <angebtes@gmail.com>
2026-03-13 23:30:02 -0600

    pipes

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,12 +14,6 @@ async function bootstrap() {
     credentials: true,
   });
   
-  app.useGlobalPipes(
-  new ValidationPipe({
-    whitelist: true,
-    transform: true,
-  }),
-);
 
   const port = process.env.PORT || 3000;
   await app.listen(port);
